79e66201b26d3ac6076fbff3dc6d4d8763d9657d
PengShengjieDev <speng@fintechautomation.com>
2023-05-23 16:39:03 +0800

    first commit

---
+++ guess-number/target/debug/deps/guess_number-1329e10709c3932d.d
@@ -0,0 +1,5 @@
+C:\Users\Administrator\Desktop\my-project\learn-rust\guess-number\target\debug\deps\guess_number-1329e10709c3932d.rmeta: src\main.rs
+
+C:\Users\Administrator\Desktop\my-project\learn-rust\guess-number\target\debug\deps\guess_number-1329e10709c3932d.d: src\main.rs
+
+src\main.rs:
---
+++ guess-number/target/debug/deps/guess_number-1f4fee993125ad82.d
@@ -0,0 +1,5 @@
+C:\Users\Administrator\Desktop\my-project\learn-rust\guess-number\target\debug\deps\guess_number-1f4fee993125ad82.rmeta: src\main.rs
+
+C:\Users\Administrator\Desktop\my-project\learn-rust\guess-number\target\debug\deps\guess_number-1f4fee993125ad82.d: src\main.rs
+
+src\main.rs:
